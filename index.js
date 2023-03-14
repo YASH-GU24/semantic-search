236627460ffb45e6b54ee14107faf98f8305c3c9
YASH-GU24 <yashg.it.20@nitj.ac.in>
2023-03-14 23:05:24 +0530

    http -> https

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const dotenv = require("dotenv");
 dotenv.config();
 
 const client = weaviate.client({
-  scheme: 'http',
+  scheme: 'https',
   host: process.env.WEAVIATE_TOKEN,
   headers: {'X-OpenAI-Api-Key': process.env.OPEN_AI_TOKEN},
   authClientSecret: new weaviate.AuthUserPasswordCredentials({
